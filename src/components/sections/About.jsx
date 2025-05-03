3a9f9c776a9a8e5158b43b01f2a6a72d9f73d343
bhupesh-roushan <roushan.bhupesh@gmail.com>
2025-05-03 17:17:29 +0530

    Experience Updated

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -37,6 +37,8 @@ import { PiSpinner } from "react-icons/pi";
 import { MdDataObject, MdOutlineSecurity } from "react-icons/md";
 import { LuComputer } from "react-icons/lu";
 import { CgWebsite } from "react-icons/cg";
+import adda from "../../assets/adda.png";
+import prep from "../../assets/prep.png";
 export const About = () => {
   const frontendSkills = [
     { name: "HTML", icon: <FaHtml5 className="text-orange-500 text-2xl" /> },
@@ -44,15 +46,27 @@ export const About = () => {
     { name: "React.js", icon: <FaReact className="text-blue-500 text-2xl" /> },
     { name: "Context API", icon: <TbApi className="text-blue-500 text-2xl" /> },
     { name: "Redux", icon: <SiRedux className="text-red-500 text-2xl" /> },
-    { name: "Tailwind CSS", icon: <SiTailwindcss className="text-cyan-500 text-2xl" /> },
-    { name: "Bootstrap", icon: <SiBootstrap className="text-purple-500 text-2xl" /> },
+    {
+      name: "Tailwind CSS",
+      icon: <SiTailwindcss className="text-cyan-500 text-2xl" />,
+    },
+    {
+      name: "Bootstrap",
+      icon: <SiBootstrap className="text-purple-500 text-2xl" />,
+    },
     { name: "Next.js", icon: <SiNextdotjs className="text-white text-2xl" /> },
-    { name: "Framer Motion", icon: <SiFramer className="text-pink-500 text-2xl" /> },
+    {
+      name: "Framer Motion",
+      icon: <SiFramer className="text-pink-500 text-2xl" />,
+    },
   ];
 
   const backendSkills = [
     { name: "Node.js", icon: <FaNodeJs className="text-green-500 text-2xl" /> },
-    { name: "Express.js", icon: <SiExpress className="text-yellow-400 text-2xl" /> },
+    {
+      name: "Express.js",
+      icon: <SiExpress className="text-yellow-400 text-2xl" />,
+    },
     { name: "REST APIs", icon: <TbApi className="text-blue-500 text-2xl" /> },
     {
       name: "Supabase (BaaS)",
@@ -67,20 +81,38 @@ export const About = () => {
     { name: "Java", icon: <FaJava className="text-red-500 text-3xl" /> },
     { name: "JavaScript", icon: <FaJs className="text-yellow-500 text-2xl" /> },
     { name: "Python", icon: <SiPython className="text-blue-500 text-2xl" /> },
-    { name: "C Programming", icon: <LiaLaptopCodeSolid className="text-green-500 text-2xl" /> },
+    {
+      name: "C Programming",
+      icon: <LiaLaptopCodeSolid className="text-green-500 text-2xl" />,
+    },
   ];
 
   const Concepts = [
-    { name: "OOPs", icon: <MdDataObject className="text-green-500 text-2xl" /> },
-    { name: "Computer Networks", icon: <LuComputer className="text-white text-2xl" /> },
-    { name: "Network Security", icon: <MdOutlineSecurity  className="text-blue-500 text-2xl" /> },
-    { name: "Software Engineering", icon: <CgWebsite  className="text-yellow-500 text-2xl" /> },
+    {
+      name: "OOPs",
+      icon: <MdDataObject className="text-green-500 text-2xl" />,
+    },
+    {
+      name: "Computer Networks",
+      icon: <LuComputer className="text-white text-2xl" />,
+    },
+    {
+      name: "Network Security",
+      icon: <MdOutlineSecurity className="text-blue-500 text-2xl" />,
+    },
+    {
+      name: "Software Engineering",
+      icon: <CgWebsite className="text-yellow-500 text-2xl" />,
+    },
   ];
 
   const DbSkills = [
-    { name: "MongoDB", icon: <SiMongodb className="text-green-500 text-2xl" /> },
+    {
+      name: "MongoDB",
+      icon: <SiMongodb className="text-green-500 text-2xl" />,
+    },
     { name: "Mongoose", icon: <SiMongoose className="text-white text-4xl" /> },
-    { name: "MySQL", icon: <GrMysql  className="text-blue-500 text-2xl" /> },
+    { name: "MySQL", icon: <GrMysql className="text-blue-500 text-2xl" /> },
   ];
 
   const toolsSkills = [
@@ -89,11 +121,23 @@ export const About = () => {
     { name: "Vercel", icon: <SiVercel className="text-white text-2xl" /> },
     { name: "Render", icon: <SiRender className="text-white text-2xl" /> },
     { name: "Netlify", icon: <SiNetlify className="text-white text-2xl" /> },
-    { name: "VS Code", icon: <DiVisualstudio className="text-blue-500 text-2xl" /> },
+    {
+      name: "VS Code",
+      icon: <DiVisualstudio className="text-blue-500 text-2xl" />,
+    },
     { name: "Figma", icon: <FaFigma className="text-pink-500 text-2xl" /> },
-    { name: "Postman", icon: <SiPostman className="text-orange-500 text-2xl" /> },
-    { name: "MySQL Workbench", icon: <SiMysql className="text-blue-500 text-4xl" /> },
-    { name: "IntelliJ IDEA", icon: <SiIntellijidea className="text-pink-500 text-2xl" /> },
+    {
+      name: "Postman",
+      icon: <SiPostman className="text-orange-500 text-2xl" />,
+    },
+    {
+      name: "MySQL Workbench",
+      icon: <SiMysql className="text-blue-500 text-4xl" />,
+    },
+    {
+      name: "IntelliJ IDEA",
+      icon: <SiIntellijidea className="text-pink-500 text-2xl" />,
+    },
   ];
 
   return (
@@ -149,7 +193,10 @@ export const About = () => {
               </div>
 
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Programming Languages</h3>
+                <h3 className="text-xl font-bold mb-4">
+                  {" "}
+                  Programming Languages
+                </h3>
                 <div className="flex flex-wrap gap-2">
                   {ProgrammingSkills.map((tech, key) => (
                     <span
@@ -216,6 +263,62 @@ export const About = () => {
           {/*  */}
 
           <div className="grid grid-cols-1  gap-6 mt-8">
+            {/* experience */}
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all hover:shadow-sm hover:shadow-blue-500/40">
+              <h3 className="text-2xl font-bold mb-4 text-orange-500 text-center">
+                Work Experience
+              </h3>
+
+              <div className="flex flex-col items-center space-y-2">
+                <div className="flex items-center space-x-2">
+                  <img
+                    src={adda}
+                    alt=""
+                    className="items-center h-12 w-12  object-cover"
+                  />
+                </div>
+
+                <h2 className="font-extrabold text-lg text-center">Adda247</h2>
+                <h2 className="font-extrabold text-lg text-center">
+                  Executive - Audit and Assessment
+                </h2>
+              </div>
+              <p className="text-center font-semibold text-md my-2 bg-gradient-to-r from-orange-600 to-blue-600 text-transparent bg-clip-text"></p>
+              <div className="my-5 ">
+                <p className="my-2 text-center text-md font-bold bg-gradient-to-r from-orange-600 to-blue-600 text-transparent bg-clip-text ">
+                  Tech Stack : Web Development, Java, Aptitude, DSA, Computer
+                  Networks, Operating Systems.
+                </p>
+
+                <p className=" font-extralight text-md text-justify">
+                  1. Developed full-stack assessment platforms using
+                  <span
+                    className="bg-gradient-to-r from-orange-600 to-blue-600 
+                      text-transparent bg-clip-text"
+                  >
+                    {" "}
+                    React, Node.js, and MongoDB, creating dynamic UIs and
+                    scalable backends; designed coding challenges and assessment
+                    questions,
+                  </span>{" "}
+                  improving evaluation quality and diversity by 30%.
+                </p>
+
+                <p className="mt-5 font-extralight text-md text-justify">
+                  2. Curated and evaluated student assessments
+                  <span
+                    className="bg-gradient-to-r from-orange-600 to-blue-600 
+                      text-transparent bg-clip-text"
+                  >
+                    {" "}
+                    focused on Operating Systems and Computer Networks,
+                    enhancing question quality and accuracy,
+                  </span>{" "}
+                  resulting in a 10% improvement in real-world problem-solving
+                  relevance.
+                </p>
+              </div>
+            </div>
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all hover:shadow-sm hover:shadow-blue-500/40">
               <h3 className="text-2xl font-bold mb-4 text-orange-500 text-center">
                 Education
@@ -261,13 +364,40 @@ export const About = () => {
                     </span>
                   </h3>
                   <p className="my-2 text-center text-md font-bold bg-gradient-to-r from-orange-600 to-blue-600 text-transparent bg-clip-text ">
-                    Tech Stack - React.js, Node.js, Express, MongoDB
+                    Tech Stack - React.js, Node.js, Express, MongoDB, JWT, REST
+                    APIs.
                   </p>
-                  <p className="text-white text-md  text-justify">
-                    Developed a ”Coursezz” template for online course selling,
-                    featuring user authentication, course listing, and database
-                    integration, using React.js, Node.js, Express, and MongoDB.
-                  </p>
+
+                  <div className="my-5 ">
+                    <p className=" font-extralight text-md text-justify">
+                      1. Developed ”Coursezz”, an online course-selling
+                      platform, implementing
+                      <span
+                        className="bg-gradient-to-r from-orange-600 to-blue-600 
+                      text-transparent bg-clip-text"
+                      >
+                        {" "}
+                        JWT-based authentication, role-based access control
+                        (RBAC), and MongoDB Atlas
+                      </span>{" "}
+                      to support secure multi-user access, reducing unauthorized
+                      access incidents.
+                    </p>
+
+                    <p className="mt-5 font-extralight text-md text-justify">
+                      2. Optimized API performance, reducing response times by
+                      40% through efficient
+                      <span
+                        className="bg-gradient-to-r from-orange-600 to-blue-600 
+                      text-transparent bg-clip-text"
+                      >
+                        {" "}
+                        Express middleware, caching strategies, and request
+                        validation,
+                      </span>{" "}
+                      enhancing scalability and system throughput.
+                    </p>
+                  </div>
                   <a
                     href="https://drive.google.com/file/d/1oFndqcViPQE_p8io7HW_wDGryYzqk7cE/view?usp=drive_link"
                     target="_blank"
