5dbd3e706076e81e7275e3a49d0ccb3d9f337321
bhupesh-roushan <roushan.bhupesh@gmail.com>
2026-02-14 01:22:04 +0530

    Loading screen updated

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -15,7 +15,7 @@ export const LoadingScreen = ({ onComplete }) => {
 
         setTimeout(() => {
           onComplete();
-        }, 1000);
+        }, 100);
       }
     }, 100);
 
@@ -27,13 +27,13 @@ export const LoadingScreen = ({ onComplete }) => {
       <div className="my-10  ">
         <img src={icon} alt="" />
       </div>
-      <div className="mb-4 text-4xl font-mono font-bold bg-gradient-to-r from-blue-600 to-orange-500 bg-clip-text text-transparent">
+      <div className="mb-4 text-4xl font-mono font-bold bg-gradient-to-r from-blue-600 to-white bg-clip-text text-transparent">
         {text} <span className="animate-blink ml-1"> | </span>
       </div>
 
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
 
-        <div className="w-[40%] h-full bg-gradient-to-r from-orange-500 to-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+        <div className="w-[40%] h-full bg-gradient-to-r from-white to-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
 
       </div>
     </div>
